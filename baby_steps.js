cc05d61168a71e3f0b11c39a033f34fc571cf7a5
Rich Seviora <richard.seviora@gmail.com>
2014-11-28 23:08:00 -0500

    Baby Steps Complete

--- baby_steps.js
+++ baby_steps.js
@@ -1,4 +1,10 @@
 /**
  * Created by richardseviora on 14-11-28.
  */
-console.log(process.argv);
+var argv = process.argv;
+argv.splice(0,2);
+var sum = 0;
+for (var i = 0; i < argv.length; i++) {
+    sum += parseFloat(argv[i]);
+}
+console.log(sum);
